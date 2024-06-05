4b4efd770479752970b92eeff1b2187b453f52e4
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2024-06-05 15:57:03 +0300

    demonstrate bean life cycle methods with annotations @PostConstruct and @PreDestroy

---
+++ 02-spring-boot-spring-core/09-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/controllers/CoachController.java
@@ -0,0 +1,24 @@
+package com.luv2code.springcoredemo.controllers;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoachController {
+
+    private Coach coach;
+
+    @Autowired
+    public CoachController(@Qualifier("cricketCoach") Coach coach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        this.coach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+}
