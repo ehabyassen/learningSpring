99e75aaceac000a56f3bd562f558222eefd80955
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2024-06-11 02:24:12 +0300

    demonstrate some spring data rest configs

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/repository/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/repository/EmployeeRepository.java
@@ -3,5 +3,7 @@ package com.luv2code.springboot.cruddemo.repository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Changes the exposed endpoints to /members
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
