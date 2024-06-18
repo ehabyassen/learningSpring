0a89eee145938c85d7b4c8d6d0fc0f48222b3f73
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2024-06-18 13:10:07 +0300

    demonstrate data binding for text fields and radio buttons

--- 05-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 05-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,18 +1,29 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${favoriteLanguages}")
+    private List<String> favoriteLanguages;
 
     @GetMapping("/showStudentForm")
     public String showStudentForm(Model model) {
         model.addAttribute("student", new Student());
+        model.addAttribute("countries", countries);
+        model.addAttribute("favoriteLanguages", favoriteLanguages);
         return "student-form";
     }
 
