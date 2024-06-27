ebc98cdb22fbdfc63e2228e2b9dfb48cd06899b6
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2024-06-27 21:17:31 +0300

    reading users and roles from the database

--- 07-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 07-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -4,14 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /**
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -34,8 +41,9 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+    */
 
-    @Bean
+                                                     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(configurer -> configurer
                         .requestMatchers("/").hasRole("EMPLOYEE")
