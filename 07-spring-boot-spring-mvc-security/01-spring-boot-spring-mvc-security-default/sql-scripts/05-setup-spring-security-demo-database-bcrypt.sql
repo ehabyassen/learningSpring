3ba3e0679403e566fca289e170f7acdd6e4355be
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2024-06-29 11:59:33 +0300

    add support for spring mvc security with custom tables

--- 07-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/sql-scripts/05-setup-spring-security-demo-database-bcrypt.sql
+++ 07-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/sql-scripts/05-setup-spring-security-demo-database-bcrypt.sql
@@ -1,11 +1,7 @@
-USE `employee_directory`;
+USE `employees`;
 
 DROP TABLE IF EXISTS `authorities`;
 DROP TABLE IF EXISTS `users`;
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
@@ -14,26 +10,11 @@ CREATE TABLE `users` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Inserting data for table `users`
---
--- NOTE: The passwords are encrypted using BCrypt
---
--- A generation tool is avail at: https://www.luv2code.com/generate-bcrypt-password
---
--- Default passwords here are: fun123
---
-
 INSERT INTO `users` 
 VALUES 
 ('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
 ('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
 ('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
-
-
---
--- Table structure for table `authorities`
---
 
 CREATE TABLE `authorities` (
   `username` varchar(50) NOT NULL,
@@ -41,10 +22,6 @@ CREATE TABLE `authorities` (
   UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
   CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Inserting data for table `authorities`
---
 
 INSERT INTO `authorities` 
 VALUES 
