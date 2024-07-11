d6ae8a2afd92e698b3d86dd170ad308253cc44db
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2024-07-11 12:25:25 +0300

    using JoinPoint to get method signature and method arguments

--- 09-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopDemo/aspect/DemoLoggingAspect.java
+++ 09-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopDemo/aspect/DemoLoggingAspect.java
@@ -1,7 +1,9 @@
 package com.luv2code.aopDemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,14 @@ import org.springframework.stereotype.Component;
 public class DemoLoggingAspect {
 
     @Before("com.luv2code.aopDemo.aspect.AopExpressions.forRepositoryPackageMethodsExcludingGettersAndSetters()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method: " + signature);
+
+        for (Object arg : joinPoint.getArgs()) {
+            System.out.println("Argument: " + arg);
+        }
+
         System.out.println(getClass() + ": logging info before addAccount...1");
     }
 }
